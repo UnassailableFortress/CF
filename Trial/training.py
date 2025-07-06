53a3bd027633dc368d60bbcc0c0d238dd9226e41
UnassailableFortress <tennisstaraarush@gmail.com>
2025-07-06 13:17:12 +0400

    Add files via upload

---
+++ Trial/training.py
@@ -0,0 +1,12 @@
+from load_data import data
+from recommender import algo
+
+trainingSet = data.build_full_trainset()
+
+algo.fit(trainingSet)
+
+
+
+
+prediction = algo.predict('E', 2)
+prediction.est
